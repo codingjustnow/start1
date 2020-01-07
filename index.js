7f6224326efd9cc61e8749ad2dc9e404db104cf1
codingjustnow <59603404+codingjustnow@users.noreply.github.com>
2020-01-07 18:05:12 +0700

    create index js

---
+++ index.js
@@ -0,0 +1,11 @@
+document.body.innerHTML = `
+<h1> hello chiang mai </h1>
+<style> 
+@import url('https://fonts.googleapis.com/css?family=Gayathri&display=swap');
+body {
+  background-color: green;
+  color: orange;
+  font-family: 'Gayathri';
+}
+</style>
+`
